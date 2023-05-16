72747922806f43775df08831298d836d18960b3e
Rakhmatullo <qosimovrahmatullo006@outlook.com>
2023-05-16 13:49:06 +0500

    ++

--- bot.py
+++ bot.py
@@ -1,7 +1,23 @@
 # Import necessary modules
 import sqlite3
-from telegram import Update, ForceReply, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, MessageHandler, Filters, ConversationHandler, CommandHandler,  CallbackQueryHandler, CallbackContext
+from telegram import (
+    Update,
+    ForceReply,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+from telegram.ext import (
+    Updater,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CommandHandler,
+    CallbackQueryHandler,
+    CallbackContext,
+)
 from config import API_TOKEN
 
 # Define states of the conversation
@@ -9,13 +25,15 @@ OPTION, FACULTY, GROUP, SUMMARY = range(1, 5)
 
 # Define function to start the bot
 facilities = [
-    "1-TTJ ",
-    "2-TTJ",
-    "3-TTJ"
+    "Xorijiy filologiya ",
+    "Tabiiy fanlar",
+    "Ijtimoiy gumanitar",
+    "Aniq fanlar (texnika)",
+    "Aniq fanlar (iqtisod)",
 ]
 
-button_enter_text = "Talabalarni TTJ ga kiritish"
-button_list_text = 'TTJ lar bo\'yicha ma\'lumot'
+button_enter_text = "Litseyga ro'yxatdan o'tish"
+button_list_text = "O'quvchilar haqida ma'lumot"
 
 
 def start_bot(update: Update, context: CallbackContext):
@@ -27,39 +45,38 @@ def start_bot(update: Update, context: CallbackContext):
 
     # Create the keyboard markup with the buttons
     keyboard = ReplyKeyboardMarkup(
-        [[button_enter], [button_list]], resize_keyboard=True)
+        [[button_enter], [button_list]], resize_keyboard=True
+    )
 
     # Send a message with the keyboard markup
     if update.message:
-        update.message.reply_text(
-            'Menyuni tanlang',  reply_markup=keyboard)
+        update.message.reply_text("Menyuni tanlang", reply_markup=keyboard)
     else:
-        update.callback_query.message.reply_text(
-            'Bosh menu', reply_markup=keyboard)
+        update.callback_query.message.reply_text("Bosh menu", reply_markup=keyboard)
 
 
 def start(update: Update, context: CallbackContext) -> int:
     print("run start")
     # Define the keyboard layout
     keyboard = [
-        [
-            InlineKeyboardButton(facilities[0], callback_data='1'),
-            InlineKeyboardButton(facilities[1], callback_data='2'),
-            InlineKeyboardButton(facilities[2], callback_data='3'),
-        ],
-        [InlineKeyboardButton("Bosh menyuga qaytish", callback_data='back')],
+        [InlineKeyboardButton(facilities[0], callback_data="1")],
+        [InlineKeyboardButton(facilities[1], callback_data="2")],
+        [InlineKeyboardButton(facilities[2], callback_data="3")],
+        [InlineKeyboardButton(facilities[3], callback_data="4")],
+        [InlineKeyboardButton(facilities[4], callback_data="5")],
+        [InlineKeyboardButton("Bosh menyuga qaytish", callback_data="back")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Set the reply markup with the above layout
 
     # Send the message and ask the user to choose an option
-    message = "TTJ ni tanlang:"
-    a = update.message.reply_text('.', reply_markup=ReplyKeyboardRemove())
-    context.bot.delete_message(
-        chat_id=update.message.chat_id, message_id=a.message_id)
+    message = "Yo'nalishni tanlang:"
+    a = update.message.reply_text(".", reply_markup=ReplyKeyboardRemove())
+    context.bot.delete_message(chat_id=update.message.chat_id, message_id=a.message_id)
+
     update.message.reply_text(message, reply_markup=reply_markup)
 
     # Set the next state to get the user's chosen option
+
 
 # Define function to handle user's chosen option
 
@@ -69,8 +86,9 @@ def get_option(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
     option = query.data
+    print(option)
     # Check if the user chose to not do anything
-    if option == 'back':
+    if option == "back":
         cancel(update, context)
         return ConversationHandler.END
 
@@ -78,14 +96,14 @@ def get_option(update: Update, context: CallbackContext) -> int:
         get_stats(update, context, option)
         return ConversationHandler.END
     # Ask the user for their full name
-    message = "Talabaning ismi va familiyasini kiriting"
+    message = "Ism va familiyangizni kiriting"
     query.answer()
-    query.message.reply_text(
-        text=message, reply_markup=ForceReply(selective=True))
+    query.message.reply_text(text=message, reply_markup=ForceReply(selective=True))
 
     # Set the next state to get the user's faculty name
-    context.user_data['option'] = option
+    context.user_data["option"] = option
     return OPTION
+
 
 # Define function to handle user's full name
 
@@ -95,26 +113,23 @@ def get_fullname(update: Update, context: CallbackContext) -> int:
     fullname = update.message.text
 
     # Ask the user for their faculty name
-    message = "Fakultet nomini kiriting"
+    message = "METRKA yoki paspoprt seriya raqamini kiritng"
     update.message.reply_text(message, reply_markup=ForceReply(selective=True))
 
     # Set the next state to get the user's group name
-    context.user_data['fullname'] = fullname
+    context.user_data["fullname"] = fullname
     return FACULTY
 
 
 # Define function to handle user's faculty name
 def get_faculty(update: Update, context: CallbackContext) -> int:
     # Get the user's faculty name
-    faculty = update.message.text
-
-    # Ask the user for their group name
-    message = "Kursi va guruhi nomini kiriting"
-    update.message.reply_text(message, reply_markup=ForceReply(selective=True))
+    serial_number = update.message.text
 
     # Set the next state to get the user's group name
-    context.user_data['faculty'] = faculty
+    context.user_data["serial_number"] = serial_number
     return GROUP
+
 
 # Define function to handle user's group name
 
@@ -124,20 +139,21 @@ def get_group(update: Update, context: CallbackContext) -> int:
     group = update.message.text
     data = context.user_data
     # Get the user's full name, faculty name, and group name
-    message = f"""Siz quyidagi ma'lumotlar bilan talabani  {facilities[int(data['option']) - 1]}ga qo'shmoqchisiz:
-Ismi va familiyasi: {data['fullname']}
-Fakultet nomi: {data['faculty']}
-Kursi va guruh nomi: {group}
-TTJ: {facilities[int(data['option']) - 1]}
+    message = f"""Siz quyidagi ma'lumotlar bilan ro'yxatdan o'tmoqchisiz  {facilities[int(data['option']) - 1]}ga qo'shmoqchisiz:
+Ismi va familiyangiz: {data['fullname']}
+Seria raqamingiz: {data['serial_number']}
+Fakultet nomi: {facilities[int(data['option'])]}
     """
     keyboard = [
-        [InlineKeyboardButton("Tasdiqlash", callback_data='4'),
-         InlineKeyboardButton("Bekor qilish", callback_data='5')]
+        [
+            InlineKeyboardButton("Tasdiqlash", callback_data="done"),
+            InlineKeyboardButton("Bekor qilish", callback_data="cancel_all"),
+        ]
     ]
     markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(message, reply_markup=markup)
 
-    context.user_data['group'] = group
+    context.user_data["group"] = group
     # End the conversation
     return SUMMARY
 
@@ -145,7 +161,7 @@ TTJ: {facilities[int(data['option']) - 1]}
 
 
 def summarize(update: Update, context: CallbackContext) -> int:
-    print('summarize function ran')
+    print("summarize function ran")
     # Get the user's chat ID and data from conversation context
 
     query = update.callback_query
@@ -153,40 +169,67 @@ def summarize(update: Update, context: CallbackContext) -> int:
     chat_id = query.message.chat_id
     data = context.user_data
     # Retrieve the user's input from context data
-    if query.data == "4":
+    if query.data == "done":
         message = f"""Tasdiqlandi ✅"""
         query.edit_message_text(text=message)
 
         fullname = data.get("fullname")
-        faculty = data.get("faculty")
-        group = data.get("group")
-        option = data.get('option')
+        serial_number = data.get("serial_number")
+        option = data.get("option")
+        faculty = facilities[int(option) - 1]
         # Connect to SQLite database and insert user input
         con = sqlite3.connect("users.db")
         cursor = con.cursor()
         cursor.execute(
-            "CREATE TABLE IF NOT EXISTS first_group_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, group_name TEXT)")
+            "CREATE TABLE IF NOT EXISTS xf_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, serial_number TEXT)"
+        )
         cursor.execute(
-            "CREATE TABLE IF NOT EXISTS second_group_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, group_name TEXT)")
+            "CREATE TABLE IF NOT EXISTS tf_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, serial_number TEXT)"
+        )
         cursor.execute(
-            "CREATE TABLE IF NOT EXISTS third_group_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, group_name TEXT)")
+            "CREATE TABLE IF NOT EXISTS aft_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, serial_number TEXT)"
+        )
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS afi_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, serial_number TEXT)"
+        )
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS ig_users (id INTEGER PRIMARY KEY, full_name TEXT, faculty TEXT, serial_number TEXT)"
+        )
+
         con.commit()
 
         if int(option) == 1:
             cursor.execute(
-                f"INSERT INTO first_group_users (full_name, faculty, group_name) VALUES (?, ?, ?)", (fullname, faculty, group))
+                f"INSERT INTO xf_users (full_name, faculty, serial_number) VALUES (?, ?, ?)",
+                (fullname, faculty, serial_number),
+            )
         elif int(option) == 2:
             cursor.execute(
-                f"INSERT INTO second_group_users (full_name, faculty, group_name) VALUES (?, ?, ?)", (fullname, faculty, group))
+                f"INSERT INTO tf_users (full_name, faculty, serial_number) VALUES (?, ?, ?)",
+                (fullname, faculty, serial_number),
+            )
         elif int(option) == 3:
             cursor.execute(
-                f"INSERT INTO third_group_users (full_name, faculty, group_name) VALUES (?, ?, ?)", (fullname, faculty, group))
+                f"INSERT INTO ig_users (full_name, faculty, serial_number) VALUES (?, ?, ?)",
+                (fullname, faculty, serial_number),
+            )
+        elif int(option) == 4:
+            cursor.execute(
+                f"INSERT INTO aft_users (full_name, faculty, serial_number) VALUES (?, ?, ?)",
+                (fullname, faculty, serial_number),
+            )
+        elif int(option) == 5:
+            cursor.execute(
+                f"INSERT INTO afi_users (full_name, faculty, serial_number) VALUES (?, ?, ?)",
+                (fullname, faculty, serial_number),
+            )
+
         con.commit()
         con.close()
         # Send a message to the user confirming the data was saved
-        message = f"Talaba TTJ ga joylandi ✅"
+        message = f"Arizangiz qabul qilindi ✅"
         context.bot.send_message(chat_id=chat_id, text=message)
-    elif query.data == "5":
+    elif query.data == "cancel_all":
         start_bot(update, context)
         query.edit_message_text(text="Amal bekor qilindi")
 
@@ -197,7 +240,6 @@ def summarize(update: Update, context: CallbackContext) -> int:
 
 # Define function to handle cancelling the conversation
 def cancel(update: Update, context: CallbackContext) -> int:
-
     # Get the user's chat ID and message ID
     query = update.callback_query
     query.answer()
@@ -212,14 +254,14 @@ def cancel(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 
-def stats(update: Update,  context: CallbackContext) -> None:
+def stats(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [
-            InlineKeyboardButton(facilities[0], callback_data='fc1'),
-            InlineKeyboardButton(facilities[1], callback_data='fc2'),
-            InlineKeyboardButton(facilities[2], callback_data='fc3'),
+            InlineKeyboardButton(facilities[0], callback_data="fc1"),
+            InlineKeyboardButton(facilities[1], callback_data="fc2"),
+            InlineKeyboardButton(facilities[2], callback_data="fc3"),
         ],
-        [InlineKeyboardButton("Bosh menyuga qaytish", callback_data='back')],
+        [InlineKeyboardButton("Bosh menyuga qaytish", callback_data="back")],
     ]
 
     # Set the reply markup with the above layout
@@ -227,9 +269,8 @@ def stats(update: Update,  context: CallbackContext) -> None:
 
     # Send the message and ask the user to choose an option
     message = "TTJ ni tanlang:"
-    a = update.message.reply_text('.', reply_markup=ReplyKeyboardRemove())
-    context.bot.delete_message(
-        chat_id=update.message.chat_id, message_id=a.message_id)
+    a = update.message.reply_text(".", reply_markup=ReplyKeyboardRemove())
+    context.bot.delete_message(chat_id=update.message.chat_id, message_id=a.message_id)
     update.message.reply_text(message, reply_markup=reply_markup)
 
 
@@ -244,7 +285,7 @@ class User:
 def get_stats(update: Update, context: CallbackContext, option: str) -> None:
     con = sqlite3.connect("users.db")
     cursor = con.cursor()
-    databases = ['first', 'second', 'third']
+    databases = ["first", "second", "third"]
     a = []
     for i in databases:
         cursor.execute(f"SELECT * FROM {i}_group_users")
@@ -256,17 +297,18 @@ def get_stats(update: Update, context: CallbackContext, option: str) -> None:
         message += facilities[0] + "+\n"
         for i in a[0]:
             message += f"{i.id}. {i.full_name} - {i.faculty} - {i.group_name}\n"
-    elif option == 'fc2':
+    elif option == "fc2":
         message += facilities[1] + "+\n"
         for i in a[1]:
             message += f"{i.id}. {i.full_name} - {i.faculty} - {i.group_name}\n"
 
-    elif option == 'fc3':
+    elif option == "fc3":
         message += facilities[2] + "+\n"
         for i in a[2]:
             message += f"{i.id}. {i.full_name} - {i.faculty} - {i.group_name}\n"
     context.bot.send_message(
-        chat_id=update.callback_query.message.chat_id, text=message)
+        chat_id=update.callback_query.message.chat_id, text=message
+    )
 
 
 def main() -> None:
@@ -281,16 +323,16 @@ def main() -> None:
             GROUP: [MessageHandler(Filters.text & ~Filters.command, get_group)],
             SUMMARY: [CallbackQueryHandler(summarize)],
         },
-        fallbacks=[CallbackQueryHandler(cancel)]
+        fallbacks=[CallbackQueryHandler(cancel)],
     )
-    enter_student_handler = MessageHandler(Filters.regex(
-        f'^{button_enter_text}$'), start)
-    get_stats_handler = MessageHandler(
-        Filters.regex(f'^{button_list_text}$'), stats)
+    enter_student_handler = MessageHandler(
+        Filters.regex(f"^{button_enter_text}$"), start
+    )
+    get_stats_handler = MessageHandler(Filters.regex(f"^{button_list_text}$"), stats)
 
     dispatcher.add_handler(conv_handler)
     dispatcher.add_handler(CallbackQueryHandler(get_stats))
-    dispatcher.add_handler(CommandHandler('start', start_bot))
+    dispatcher.add_handler(CommandHandler("start", start_bot))
     dispatcher.add_handler(CommandHandler("stats", stats))
     dispatcher.add_handler(CommandHandler("add", start))
     dispatcher.add_handler(get_stats_handler)
